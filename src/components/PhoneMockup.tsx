bad506f65e0b1a00dd827dab34c9e45507e43c6a
KC Dacre8tor <kcdacre8tor@KCs-MacBook-Pro-2.local>
2025-10-24 21:50:05 -0400

    Revert phone mockup sizing changes - will update images manually

--- src/components/PhoneMockup.tsx
+++ src/components/PhoneMockup.tsx
@@ -6,14 +6,12 @@ interface PhoneMockupProps {
 
 export default function PhoneMockup({ image, alt, className = '' }: PhoneMockupProps) {
   return (
-    <div className="pb-8">
-      <img
-        src={image}
-        alt={alt}
-        className={`w-full h-auto max-w-[400px] md:max-w-[500px] lg:max-w-[600px] mx-auto block ${className}`}
-        style={{ backgroundColor: 'transparent' }}
-        loading="lazy"
-      />
-    </div>
+    <img
+      src={image}
+      alt={alt}
+      className={`w-full h-auto max-w-[400px] md:max-w-[500px] lg:max-w-[600px] mx-auto block ${className}`}
+      style={{ backgroundColor: 'transparent' }}
+      loading="lazy"
+    />
   );
 }
